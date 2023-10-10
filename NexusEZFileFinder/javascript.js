74493b3846c9970f157bea7ab17578dc2d9c822a
John Reedy <johnreedy01@gmail.com>
2023-10-10 02:33:20 +0100

    Implement search button toggle functionality for URL incrementation

--- NexusEZFileFinder/javascript.js
+++ NexusEZFileFinder/javascript.js
@@ -1,9 +1,11 @@
 const GAME_ID = "skyrimspecialedition";
 const MOD_ID = 100562;
 let FILE_ID = 1;
+let BACKUP_FILE_ID = FILE_ID;
 let URL_LINK = `https://www.nexusmods.com/${GAME_ID}/mods/${MOD_ID}?tab=files&file_id=${FILE_ID}`
+let searchingForMods = false;
 
-function createLogLinkElement () {
+function createLogElement () {
     const a = document.createElement('a');
     const link = document.createTextNode(URL_LINK);
     a.appendChild(link);
@@ -12,12 +14,18 @@ function createLogLinkElement () {
     document.getElementById('text-log').appendChild(a);
     URL_LINK = `https://www.nexusmods.com/${GAME_ID}/mods/${MOD_ID}?tab=files&file_id=${FILE_ID + 1}`;
     console.log(URL_LINK)
+    document.getElementById('text-log').scrollTop = document.getElementById('text-log').scrollHeight;
 }
 
-async function asyncCreateLogLinkElement() {
+async function asyncCreateLogElement() {
+    FILE_ID = BACKUP_FILE_ID;
     for (; FILE_ID < 1000; FILE_ID++) {
-        await new Promise(resolve => setTimeout(resolve, 10));
-        createLogLinkElement();
+        if (searchingForMods === true) {
+            await new Promise(resolve => setTimeout(resolve, 10));
+            createLogElement();
+        } else {
+            break;
+        }
     }
 }
 
@@ -32,6 +40,9 @@ SEARCH_OPTIONS_BUTTON.addEventListener("click", changeTabSearchOptions);
 
 const HELP_BUTTON = document.getElementById('help-button');
 HELP_BUTTON.addEventListener("click", changeTabHelp);
+
+const FIND_DELETED_MODS_BUTTON = document.getElementById('find-deleted-mods-button');
+FIND_DELETED_MODS_BUTTON.addEventListener("click", startModSearch);
 
 function clearAllTabs () {
     document.getElementById('find-mods').style.display = "none";
@@ -60,7 +71,24 @@ function changeTabHelp () {
     document.getElementById('help-text').style.display = "flex";
 }
 
+function startModSearch() {
+    if (searchingForMods === false) {
+        document.getElementById('find-deleted-mods-button').innerHTML = "Stop Searching";
+        searchingForMods = true;
+        asyncCreateLogElement();
+        
+    } else {
+        searchingForMods = false;
+        document.getElementById('find-deleted-mods-button').innerHTML = "Find Deleted Mods";
+    }
+}
+
 clearAllTabs();
 document.getElementById('find-mods').style.display = "flex";
 
-asyncCreateLogLinkElement();
+const TEXT_AREA = document.getElementById("find-mods");
+TEXT_AREA.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+    }
+});
